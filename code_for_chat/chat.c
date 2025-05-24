541652c6e85e5e401b038d8a8849241fb9b070de
Rogov Sergey <vboxuser@vbox.myguest.virtualbox.org>
2025-05-24 19:44:14 +0300

    CHAT: feat: Добавлена Функция клиент-серверного взаимодействия.
    Реализовывает запуск сервера или подключение клиента в зависимости от
    выбранного режима работы.

--- code_for_chat/chat.c
+++ code_for_chat/chat.c
@@ -5,11 +5,13 @@
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<arpa/inet.h>
+#include<sys/un.h>
 #include<sys/select.h>
 
 #define BUF_SIZE 256
+#define SOCKET_PATH "/tmp/chat"
 
-void startchat(int socket_fd, const char *a)
+void chat(int socket_fd, const char *a)
 {
     int max_fd;
     char buffer[BUF_SIZE];
@@ -61,11 +63,77 @@ void startchat(int socket_fd, const char *a)
             buffer[bytes_rec]='\0';
             printf(">%s",buffer);
             if (strncmp(buffer,"exit",4)==0){
-                printf("[%s] User left from the chat.\n");
+                printf("[%s] User left from the chat.\n",a);
                 break;
             }
         }
     }
+}
+
+void process_for_chatting(const char *mode)
+{
+    struct sockaddr_un server_addr;
+    int socket_fd;
+    memset(&server_addr,0,sizeof(server_addr));
+    server_addr.sun_family=AF_UNIX;
+    strncpy (server_addr.sun_path, SOCKET_PATH,sizeof(server_addr.sun_path)-1);
+    server_addr.sun_path[sizeof(server_addr.sun_path)-1]='\0';
+
+    if(strcmp(mode, "server")==0){
+        printf("Server mode.\n");
+        int server_fd,client_fd;
+        struct sockaddr_un client_addr_info; 
+        int client_len=sizeof(client_addr_info);
+        
+        if((server_fd = socket(AF_UNIX,SOCK_STREAM,0))==-1)
+        {
+            perror("socket");
+            EXIT_FAILURE;
+        }
+        unlink(SOCKET_PATH);
+        if (bind(server_fd,(struct sockaddr*)&server_addr,sizeof(server_addr))==-1)
+        {
+            close(server_fd);
+            unlink(SOCKET_PATH);
+            perror("bind");
+            EXIT_FAILURE;
+        }
+        if (listen(server_fd,1)==-1)
+        {
+            close (server_fd);
+            unlink(SOCKET_PATH);
+            perror("listen");
+            EXIT_FAILURE;
+        }
+        printf("ready for listen!\n");
+        if ((socket_fd=accept(server_fd,(struct sockaddr*)&client_addr_info,&client_len))==-1)
+        {
+            close(server_fd);
+            unlink(SOCKET_PATH);
+            perror("client");
+            EXIT_FAILURE;
+        }
+        printf("client on the server\n");
+        chat(socket_fd,"client");
+        
+    } else if (strcmp(mode,"client")==0){
+        printf("client mode\n");
+        int client_sockfd;
+        if((client_sockfd=socket(AF_UNIX,SOCK_STREAM,0))==-1)
+        {
+            perror("socket (client)\n");
+            EXIT_FAILURE;
+        }
+        printf("connecting to the server...\n");
+        if(connect(client_sockfd,(struct sockaddr*)&server_addr,sizeof(server_addr))==-1){
+            close(client_sockfd);
+            perror("connect (client\n");
+            EXIT_FAILURE;
+        }
+        printf("complete!\n");
+        chat(client_sockfd,"server");
+    }
+
 }
 
 int main(int argc ,char *argv[])
